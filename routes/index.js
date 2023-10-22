6f2dafdc5995040038cfb31135af9061eb2236f5
tgaytan <tgaytan14@utexas.edu>
2023-10-22 18:27:04 -0500

    added code to delete notes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const api = require('express').Router();
-const data = require('../db/db.json');
+let jsonData = require('../db/db.json');
 const fs = require('fs');
 const generateUniqueId = require('generate-unique-id');
 
@@ -11,23 +11,33 @@ api.use(express.urlencoded({ extended: true }));
 
 api.get('/notes', (req, res) => {
     // res.send('GET to /api/notes works now');
-    res.json(data);
+    fs.readFile(`${__dirname}/../db/db.json`, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else { 
+            jsonData = JSON.parse(data);
+        }
+     });
+    //  console.log('will this show--------');
+    //  console.log(jsonData);
+
+    res.json(jsonData);
     // console.log(__dirname);
     // res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 api.post('/notes', (req, res) => {
     // console.log(typeof req.body);
-    data.push(req.body);
+    jsonData.push(req.body);
     // console.log(data);
 
-    for (const aNote of data) {
+    for (const aNote of jsonData) {
         aNote.id = generateUniqueId();
     }
 
-    dataString = JSON.stringify(data);
-    console.log(data);
-    console.log(dataString);
+    dataString = JSON.stringify(jsonData);
+    // console.log(jsonData);
+    // console.log(dataString);
     // console.log(typeof dataString);
     // res.send('POST to /notes works now');
     // console.log(__dirname);
@@ -39,7 +49,17 @@ api.post('/notes', (req, res) => {
     res.send('post successful');
 });
 
-// api.delete(`/api/notes/${id}`, (req, res) => {
-// });
+api.delete(`/notes/:id`, (req, res) => {
+    // console.log(req.params.id)
+    for (const aNote of jsonData) {
+        if (req.params.id === aNote.id) {
+            console.log('matching id');
+            const index = jsonData.indexOf(aNote);
+            // console.log(index);
+            jsonData.splice(index, 1);
+        }
+    }
+    res.send('sending text for now');
+});
 
 module.exports = api;
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const apiRoute = require('./routes');
 
 app.use(express.static('public'));
 
-
 app.use('/api', apiRoute);
 
 app.get('/notes', (req, res) => {
@@ -14,6 +13,8 @@ app.get('/notes', (req, res) => {
     // console.log(__dirname);
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, () => 
     console.log('Listening on http://localhost:3001')
