1db91caab7840b6431be7a87c4285ad09394974c
tgaytan <tgaytan14@utexas.edu>
2023-10-22 15:34:25 -0500

    corrected code for the GET request. it's only to send notes.html and does not get data

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-const api = require('express').Router();
-const path = require('path');
+const notes = require('express').Router();
+// const path = require('path');
 
-api.get('/notes', (req, res) => {
+notes.get('/', (req, res) => {
     // res.send('GET to /notes works now');
-    console.log(__dirname);
+    // console.log(__dirname);
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-api.post('/notes', (req, res) => {
-    res.send('POST to /notes works now');
-});
-
-// api.delete(`/api/notes/${id}`, (req, res) => {
+// notes.post('/', (req, res) => {
+//     res.send('POST to /notes works now');
 // });
 
-module.exports = api;
+// notes.delete(`/api/notes/${id}`, (req, res) => {
+// });
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const apiRoute = require('./routes');
+const path = require('path');
+// const notesRoute = require('./routes');
 
 app.use(express.static('public'));
 
-app.use('/api', apiRoute);
+// app.use('/notes', notesRoute);
+
+app.get('/notes', (req, res) => {
+    // res.send('GET to /notes works now');
+    // console.log(__dirname);
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
 app.listen(PORT, () => 
     console.log('Listening on http://localhost:3001')
